3e593947e8ce9107b6ac78644946fdb003179250
naimcaceres1 <naim.caceres1@gmail.com>
2026-02-11 01:01:10 -0300

    first commit

---
+++ src/main/java/com/ecommerce/campestre/CampestreApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.campestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/campestre/CampestreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.campestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
